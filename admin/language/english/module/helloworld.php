a9ba1e6314b91fedc2b844b0b0c2d37a915d91df
Maria Vilaró <maria.vilaro@gmail.com>
2015-01-14 00:17:04 +0100

    First commit

---
+++ admin/language/english/module/helloworld.php
@@ -0,0 +1,23 @@
+<?php
+// Heading
+$_['heading_title']       = 'Hello World';
+ 
+// Text
+$_['text_module']         = 'Modules';
+$_['text_edit']           = 'Edit Hello World Module';
+$_['text_success']        = 'Success: You have modified module Hello World!';
+$_['text_content_top']    = 'Content Top';
+$_['text_content_bottom'] = 'Content Bottom';
+$_['text_column_left']    = 'Column Left';
+$_['text_column_right']   = 'Column Right';
+ 
+// Entry
+$_['entry_code']          = 'Hello World Code:';
+$_['entry_layout']        = 'Layout:';
+$_['entry_position']      = 'Position:';
+$_['entry_status']        = 'Status:';
+$_['entry_sort_order']    = 'Sort Order:';
+ 
+// Error
+$_['error_permission']    = 'Warning: You do not have permission to modify module Hello World!';
+$_['error_code']          = 'Code Required';
